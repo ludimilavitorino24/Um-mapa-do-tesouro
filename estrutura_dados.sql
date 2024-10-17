232f91aa15eed60e9746907e2aee12a0550d562c
Roberto Mota Besser <rmbesser@gmail.com>
2024-10-16 22:41:31 -0300

    Update and rename bagaça.sql to estrutura_dados.sql
    Atualização do script de DB.

--- estrutura_dados.sql
+++ estrutura_dados.sql
@@ -1,7 +1,7 @@
 /*
 ALTER TABLE "t_sensors" DROP CONSTRAINT "FK_t_sensors__sensor_types";
-ALTER TABLE "t_output_types" DROP CONSTRAINT "FK__output_types__sensor_types";
-ALTER TABLE "t_output_types" DROP CONSTRAINT "FK__output_types_t_types";
+ALTER TABLE "t_output_sensor_types" DROP CONSTRAINT "FK__sensor_types__sensor_types";
+ALTER TABLE "t_output_sensor_types" DROP CONSTRAINT "FK__sensor_types_t_types";
 ALTER TABLE "t_sensors_log" DROP CONSTRAINT "FK_t_sensors_log_t_sensors";
 ALTER TABLE "t_sensors_log" DROP CONSTRAINT "FK_t_sensors_log_t_types";
 ALTER TABLE "t_recommendations" DROP CONSTRAINT "FK_commendations_t_inputs";
@@ -11,8 +11,8 @@ DROP TABLE "t_sensors" PURGE;
 DROP SEQUENCE "SQ_t_sensors";
 DROP TABLE "t_sensor_types" PURGE;
 DROP SEQUENCE "SQ_t_sensor_types";
-DROP TABLE "t_output_types" PURGE;
-DROP SEQUENCE "SQ_t_output_types";
+DROP TABLE "t_output_sensor_types" PURGE;
+DROP SEQUENCE "SQ_t_output_sensor_types";
 DROP TABLE "t_sensors_log" PURGE;
 DROP SEQUENCE "SQ_t_sensors_log";
 DROP TABLE "t_types" PURGE;
@@ -30,9 +30,9 @@ DROP SEQUENCE "SQ_t_applications_log";
 -------------------------------------------------------------------------------
 
 CREATE TABLE "t_sensors" (
-    "id"                              INTEGER             NOT NULL
+    "id"                              NUMBER              NOT NULL
   , "name"                            VARCHAR2(50)        NOT NULL
-  , "type_id"                         INTEGER             NOT NULL
+  , "sensor_type_id"                  NUMBER              NOT NULL
   , CONSTRAINT "PK_t_sensors" PRIMARY KEY ( "id" )
 );
 
@@ -56,9 +56,9 @@ SHOW ERRORS;
 -------------------------------------------------------------------------------
 
 CREATE TABLE "t_sensor_types" (
-    "id"                              INTEGER             NOT NULL
+    "id"                              NUMBER              NOT NULL
   , "name"                            VARCHAR2(50)        NOT NULL
-  , "qtd_outputs"                     INTEGER
+  , "qtd_outputs"                     NUMBER
   , CONSTRAINT "PK_t_sensor_types" PRIMARY KEY ( "id" )
 );
 
@@ -78,26 +78,26 @@ END;
 SHOW ERRORS;
 
 -------------------------------------------------------------------------------
---            t_output_types
+--            t_output_sensor_types
 -------------------------------------------------------------------------------
 
-CREATE TABLE "t_output_types" (
-    "id"                              INTEGER             NOT NULL
+CREATE TABLE "t_output_sensor_types" (
+    "id"                              NUMBER              NOT NULL
   , "name"                            VARCHAR2(50)        NOT NULL
-  , "sensor_type_id"                  INTEGER             NOT NULL
-  , "type_id"                         INTEGER             NOT NULL
-  , CONSTRAINT "PK_t_output_types" PRIMARY KEY ( "id" )
+  , "sensor_type_id"                  NUMBER              NOT NULL
+  , "type_id"                         NUMBER              NOT NULL
+  , CONSTRAINT "PK_t_output_sensor_types" PRIMARY KEY ( "id" )
 );
 
 
-CREATE SEQUENCE "SQ_t_output_types";
+CREATE SEQUENCE "SQ_t_output_sensor_types";
 
-CREATE OR REPLACE TRIGGER "TG_t_output_types_BI"
-    BEFORE INSERT ON "t_output_types"
+CREATE OR REPLACE TRIGGER "TG_t_output_sensor_types_BI"
+    BEFORE INSERT ON "t_output_sensor_types"
     FOR EACH ROW
 BEGIN
     if :NEW."id" is NULL then
-        :NEW."id" := "SQ_t_output_types".nextVal;
+        :NEW."id" := "SQ_t_output_sensor_types".nextVal;
     end if;
 END;
 /
@@ -109,10 +109,10 @@ SHOW ERRORS;
 -------------------------------------------------------------------------------
 
 CREATE TABLE "t_sensors_log" (
-    "id"                              INTEGER             NOT NULL
-  , "sensor_id"                       INTEGER
+    "id"                              NUMBER              NOT NULL
+  , "sensor_id"                       NUMBER
   , "value"                           DOUBLE PRECISION
-  , "type_id"                         INTEGER             NOT NULL
+  , "type_id"                         NUMBER              NOT NULL
   , "latitude"                        DECIMAL
   , "longitude"                       DOUBLE PRECISION
   , "created_at"                      TIMESTAMP
@@ -139,7 +139,7 @@ SHOW ERRORS;
 -------------------------------------------------------------------------------
 
 CREATE TABLE "t_types" (
-    "id"                              INTEGER
+    "id"                              NUMBER
   , "name"                            VARCHAR2(50)
   , CONSTRAINT "PK_t_types" PRIMARY KEY ( "id" )
 );
@@ -164,9 +164,9 @@ SHOW ERRORS;
 -------------------------------------------------------------------------------
 
 CREATE TABLE "t_recommendations" (
-    "id"                              INTEGER             NOT NULL
-  , "input_id"                        INTEGER
-  , "input_qtd"                       INTEGER
+    "id"                              NUMBER              NOT NULL
+  , "input_id"                        NUMBER
+  , "input_qtd"                       NUMBER
   , "latitude"                        DOUBLE PRECISION
   , "longitude"                       DOUBLE PRECISION
   , "created_at"                      DATE
@@ -193,7 +193,7 @@ SHOW ERRORS;
 -------------------------------------------------------------------------------
 
 CREATE TABLE "t_inputs" (
-    "id"                              INTEGER             NOT NULL
+    "id"                              NUMBER              NOT NULL
   , "name"                            VARCHAR2(50)
   , CONSTRAINT "PK_t_inputs" PRIMARY KEY ( "id" )
 );
@@ -218,9 +218,9 @@ SHOW ERRORS;
 -------------------------------------------------------------------------------
 
 CREATE TABLE "t_applications_log" (
-    "id"                              INTEGER             NOT NULL
-  , "recommendation_id"               INTEGER
-  , "input_id"                        INTEGER             NOT NULL
+    "id"                              NUMBER              NOT NULL
+  , "recommendation_id"               NUMBER
+  , "input_id"                        NUMBER              NOT NULL
   , "input_qtd"                       FLOAT               NOT NULL
   , "latitude"                        DOUBLE PRECISION
   , "longitude"                       DOUBLE PRECISION
@@ -245,9 +245,9 @@ SHOW ERRORS;
 
 -------------------------------------------------------------------------------
 
-ALTER TABLE "t_sensors" ADD CONSTRAINT "FK_t_sensors__sensor_types" FOREIGN KEY ( "type_id" ) REFERENCES "t_sensor_types" ( "id" );
-ALTER TABLE "t_output_types" ADD CONSTRAINT "FK__output_types__sensor_types" FOREIGN KEY ( "sensor_type_id" ) REFERENCES "t_sensor_types" ( "id" );
-ALTER TABLE "t_output_types" ADD CONSTRAINT "FK__output_types_t_types" FOREIGN KEY ( "type_id" ) REFERENCES "t_types" ( "id" );
+ALTER TABLE "t_sensors" ADD CONSTRAINT "FK_t_sensors__sensor_types" FOREIGN KEY ( "sensor_type_id" ) REFERENCES "t_sensor_types" ( "id" );
+ALTER TABLE "t_output_sensor_types" ADD CONSTRAINT "FK__sensor_types__sensor_types" FOREIGN KEY ( "sensor_type_id" ) REFERENCES "t_sensor_types" ( "id" );
+ALTER TABLE "t_output_sensor_types" ADD CONSTRAINT "FK__sensor_types_t_types" FOREIGN KEY ( "type_id" ) REFERENCES "t_types" ( "id" );
 ALTER TABLE "t_sensors_log" ADD CONSTRAINT "FK_t_sensors_log_t_sensors" FOREIGN KEY ( "sensor_id" ) REFERENCES "t_sensors" ( "id" );
 ALTER TABLE "t_sensors_log" ADD CONSTRAINT "FK_t_sensors_log_t_types" FOREIGN KEY ( "type_id" ) REFERENCES "t_types" ( "id" );
 ALTER TABLE "t_recommendations" ADD CONSTRAINT "FK_commendations_t_inputs" FOREIGN KEY ( "input_id" ) REFERENCES "t_inputs" ( "id" );
